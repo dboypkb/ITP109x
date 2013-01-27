908e7b18d3d1b85030635072563947b2898842d7
Darren Pouliot <dboypkb@gmail.com>
2013-01-26 16:04:55 -0800

    Initial and Final Commit

---
+++ Lab1/InstructionClass.java
@@ -0,0 +1,21 @@
+
+/* 
+ *  Darren Pouliot
+ *  dpouliot@usc.edu
+ *  ITP 109 - Spring 2013
+ *  Lab 1: Prints 5 Instructions on Opening Eclipse to the Console
+ */
+
+public class InstructionClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Step 1: Open the Applications folder");
+		System.out.println("Step 2: Double-click the Eclipse icon");
+		System.out.println("Step 3: Go to File > New > Java Project");
+		System.out.println("Step 4: Go to File > New > Class");
+		System.out.println("Step 5: Begin Coding!");
+		
+	}
+
+}
